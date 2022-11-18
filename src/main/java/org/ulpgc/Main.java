ad9eb8fb9c5171d0a007e061181f4e8d1940f789
Jan Maděra <jan.madera.12@gmail.com>
2022-11-18 20:32:42 +0000

    Add spark sql

--- src/main/java/org/ulpgc/Main.java
+++ src/main/java/org/ulpgc/Main.java
@@ -1,16 +1,32 @@
 package org.ulpgc;
 
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.ulpgc.crawler.Crawler;
 import org.ulpgc.invertedIndex.InvertedIndex;
 import static spark.Spark.*;
+import org.apache.spark.sql.SparkSession;
 
 public class Main {
+
     public static void main(String[] args) {
 
-        get("/hello", (req, res) -> "Hello World");
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("Java Spark SQL basic example")
+                .config("spark.master", "local")
+                .getOrCreate();
 
+        Dataset<Row> df = spark.read().json("words.json");
+
+        df.show();
+
+        get("/hello", (req, res) -> "Hello World!");
+
+//        df.show();
+//
 //        new Crawler();
-
+//
 //        new InvertedIndex();
     }
 }
